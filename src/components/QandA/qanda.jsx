389a59bfcb4700c3128265c3822d3db9e7958c6d
Rosita Calderon <rpcalderon86@gmail.com>
2018-11-14 21:27:23 -0500

    move everything out of index

---
+++ src/components/QandA/qanda.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import styled from 'styled-components';
+import QandaList from './QandaList.jsx';
+
+class Qanda extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      attraction: [],
+      questions: []
+    };
+  }
+
+  componentDidMount() {
+    $.get('/api/attraction/1', data => {
+      this.setState({ attraction: data });
+    });
+    $.get('/api/questions/1', data => {
+      console.log('data', data);
+      this.setState({ questions: data });
+    });
+  }
+
+  askQuestion(question) {
+    // console.log(`${question} was asked.`);
+    // console.log(this);
+    var that = this;
+    // TODO
+    $.post('/api/question/1/50', { question: question }, function(data) {
+      console.log('data post', data);
+      // let dataParsed = JSON.parse(data);
+      that.setState({ questions: data });
+    });
+  }
+
+  render() {
+    // console.log('render', this.state.questions);
+    return (
+      <div>
+        <h1>{this.state.attraction.name}</h1>
+        <QandaList
+          data={this.state.questions}
+          askQuestion={this.askQuestion.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+export default Qanda;
--- src/index.jsx
+++ src/index.jsx
@@ -2,48 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import styled from 'styled-components';
-import QandaList from './components/QandA/QandaList.jsx';
+import Qanda from './components/QandA/Qanda.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      attraction: [],
-      questions: []
-    };
-  }
-
-  componentDidMount() {
-    $.get('/api/attraction/1', data => {
-      this.setState({ attraction: data });
-    });
-    $.get('/api/questions/1', data => {
-      console.log('data', data);
-      this.setState({ questions: data });
-    });
-  }
-
-  askQuestion(question) {
-    // console.log(`${question} was asked.`);
-    // console.log(this);
-    var that = this;
-    // TODO
-    $.post('/api/question/1/50', { question: question }, function(data) {
-      console.log('data post', data);
-      // let dataParsed = JSON.parse(data);
-      that.setState({ questions: data });
-    });
+    this.state = {};
   }
 
   render() {
     // console.log('render', this.state.questions);
     return (
       <div>
-        <h1>{this.state.attraction.name}</h1>
-        <QandaList
-          data={this.state.questions}
-          askQuestion={this.askQuestion.bind(this)}
-        />
+        <Qanda />
       </div>
     );
   }
